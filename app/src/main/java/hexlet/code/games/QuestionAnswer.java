84283e392c827b645686262e6da3ca0e3e13e31e
Saplitsyn Denis <packman17@mail.ru>
2023-09-18 22:10:53 +0300

    create class QuestionAnswer

---
+++ app/src/main/java/hexlet/code/games/QuestionAnswer.java
@@ -0,0 +1,19 @@
+package hexlet.code.games;
+
+public class QuestionAnswer {
+    String question;
+    String answer;
+
+    public QuestionAnswer(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
