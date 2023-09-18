7909703b6a8b75e5c92849b85de67a5bef1b3aae
Saplitsyn Denis <packman17@mail.ru>
2023-09-18 22:17:56 +0300

    refactor run game

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,66 +1,42 @@
 package hexlet.code;
 
-import hexlet.code.games.Even;
-import hexlet.code.games.Calculator;
-import hexlet.code.games.Gcd;
-import hexlet.code.games.Progression;
-import hexlet.code.games.Prime;
+import hexlet.code.games.Game;
+import hexlet.code.games.QuestionAnswer;
 
 import java.util.Scanner;
 
-import static hexlet.code.App.EVEN;
-import static hexlet.code.App.CALC;
-import static hexlet.code.App.GCD;
-import static hexlet.code.App.PROGRESSION;
-import static hexlet.code.App.PRIME;
-
 public class Engine {
     static final int NUMBER_OF_ROUND = 3;
-    static final int NUMBER_WIN_ANSWER = 3;
 
-    public static void playGame(int userChoice) {
+    public Engine(Game game) {
+        int countWinAnswer = 0;
+
         Scanner scanner = new Scanner(System.in);
 
-        String correctAnswer = "";
-        int countWinAnswer = 0;
-        String userAnswer;
+        Cli.greetingUser();
+        System.out.println(game.getRule());
 
         for (int i = 0; i < NUMBER_OF_ROUND; i++) {
-            switch (userChoice) {
-                case EVEN:
-                    correctAnswer = Even.gameCorrectAnswer();
-                    break;
-                case CALC:
-                    correctAnswer = Calculator.gameCorrectAnswer();
-                    break;
-                case GCD:
-                    correctAnswer = Gcd.gameCorrectAnswer();
-                    break;
-                case PROGRESSION:
-                    correctAnswer = Progression.gameCorrectAnswer();
-                    break;
-                case PRIME:
-                    correctAnswer = Prime.gameCorrectAnswer();
-                    break;
-                default:
-                    System.out.println("no such game: " + userChoice);
-                    break;
-            }
-            userAnswer = scanner.nextLine();
+            QuestionAnswer quiz = game.getQuestionAnswer();
+            String question = quiz.getQuestion();
+            String answer = quiz.getAnswer();
 
-            if (userAnswer.equals(correctAnswer)) {
-                System.out.println("Correct!");
+            System.out.println("Question: " + question);
+
+            String userAnswer = scanner.nextLine();
+
+            if (userAnswer.equalsIgnoreCase(answer)) {
                 countWinAnswer++;
+                System.out.println("Correct!");
             } else {
-                System.out.println(userAnswer + " is wrong answer ;(. Correct answer was " + correctAnswer);
+                System.out.println(userAnswer + " is wrong answer ;(. Correct answer was " + answer);
                 System.out.println("Let's try again, " + Cli.userName + "!");
                 break;
             }
-
-            if (countWinAnswer == NUMBER_WIN_ANSWER) {
-                System.out.println("Congratulations, " + Cli.userName + "!");
-            }
         }
-        scanner.close();
+
+        if (countWinAnswer == NUMBER_OF_ROUND) {
+            System.out.println("Congratulations, " + Cli.userName + "!");
+        }
     }
 }
