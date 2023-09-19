96df727d07751963afc2deaf672ba2443adb14b0
Saplitsyn Denis <packman17@mail.ru>
2023-09-19 14:24:33 +0300

    changes for checkstyle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,14 +1,15 @@
 package hexlet.code;
 
 import java.util.Scanner;
+
 public class Cli {
 
-   public static void greetingUser() {
-       Scanner scanner = new Scanner(System.in);
+    public static void greetingUser() {
+        Scanner scanner = new Scanner(System.in);
 
-       System.out.println("Welcome to the Brain Games!");
-       System.out.println("May I have your name?");
-       String userName = scanner.nextLine();
-       System.out.println("Hello, " + userName + "!");
-   }
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        String userName = scanner.nextLine();
+        System.out.println("Hello, " + userName + "!");
+    }
 }
